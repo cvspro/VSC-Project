d9c7853eb29147b9b1bf82b528d1fb5d1c0f2310
cvspro <a08358682@gmail.com>
2024-01-28 19:06:48 +0530

    created dataframe for use cases

--- pythonhello.py
+++ pythonhello.py
@@ -1,7 +1,4 @@
 import pandas as pd
 import numpy as np
-# df=pd.DataFrame(np.random.randn(6,4),columns=list("ABCD"),index=[1,2,3,4,5,6])
-# print(df)
-
-for i in range(1,6):
-    print(i)
+df=pd.DataFrame(np.random.randn(6,4),columns=list("ABCD"),index=[1,2,3,4,5,6])
+print(df)
