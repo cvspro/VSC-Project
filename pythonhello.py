c0c35ef2644d4540018932ecff6b3b6f4f3a99d8
cvspro <a08358682@gmail.com>
2024-01-28 20:08:12 +0530

    added column D

--- pythonhello.py
+++ pythonhello.py
@@ -3,7 +3,6 @@ import numpy as np
 # df=pd.DataFrame(np.random.randn(6,4),columns=list("ABCD"),index=[1,2,3,4,5,6])
 df=pd.DataFrame({
     "A":np.array([3]*4),
-    "B":pd.Series(1,index=list(range(4)),dtype='float32'),
-    "C":pd.Timestamp("20230102")
+    "B":pd.Series(1,index=list(range(4)),dtype='float32')
 })
 print(df)
