147a0aa26289196c7d09f045208484d5a629f3f2
cvspro <a08358682@gmail.com>
2024-01-28 19:11:29 +0530

    new changed happen in python file

--- pythonhello.py
+++ pythonhello.py
@@ -1,4 +1,7 @@
 import pandas as pd
 import numpy as np
-df=pd.DataFrame(np.random.randn(6,4),columns=list("ABCD"),index=[1,2,3,4,5,6])
+# df=pd.DataFrame(np.random.randn(6,4),columns=list("ABCD"),index=[1,2,3,4,5,6])
+df=pd.DataFrame({
+    "A":pd.Series([3]*4)
+})
 print(df)
